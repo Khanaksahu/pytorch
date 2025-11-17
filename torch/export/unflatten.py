caa9325c71b72eb36d6a01709d9032fd6a3f781c
Malay Bag <malaybag@meta.com>
2025-11-16 20:38:48 -0800

    [torch.export] Copy common custom metadata of children node to parent (call_module) node (#167952)
    Summary:
    
    Same module can be used with and without dynamo disabled flag. So instead of marking the module as dynamo disabled, marking the call module which dynamo disabled. This can introduce a new submodule to encapsulate dynamo disabled nodes. For that reason, prune_pytree_flatten_unflatten need to be updated.
    
    Test Plan:
    ```
    buck test mode/opt caffe2/test:test_export -- 'test_uplift_common_custom_meta'
    ```
    
    Differential Revision: D86354586

--- torch/export/unflatten.py
+++ torch/export/unflatten.py
@@ -1278,6 +1278,23 @@ class _ModuleFrame:
                 f"Could not run remap_input() on op type: {x.op} for node {x}"
             )
 
+    def uplift_common_custom_metadata(self) -> None:
+        # Copy custom metadata if all nodes have same custom metadata
+        custom_meta = {}
+        for node in self.node_map.values():
+            if "custom" in node.meta and not custom_meta:
+                custom_meta = node.meta["custom"]
+            elif "custom" not in node.meta or custom_meta != node.meta["custom"]:
+                custom_meta = {}
+                break
+
+        if custom_meta:
+            # Lift common custom metadata to parent node and clear children node's custom metadata
+            assert self.parent_call_module is not None
+            self.parent_call_module.meta["custom"] = custom_meta
+            for node in self.node_map.values():
+                del node.meta["custom"]
+
     def finalize_outputs(self):
         self.created_modules.pop(self.fqn, None)
 
@@ -1356,6 +1373,7 @@ class _ModuleFrame:
             if isinstance(graph_outputs, torch.fx.Node)
             else [o.meta.get("val") for o in graph_outputs]
         )
+        self.uplift_common_custom_metadata()
 
         if len(orig_outputs) == 1 and signature is None:
             self.parent.node_map[orig_outputs[0]] = parent_out
